fea1e58e0555876d39815c2d1938df604a826ccf
andilupu92 <andilupu92@gmail.com>
2026-01-26 18:14:01 +0200

    Initial commit

---
+++ src/main/java/auto/trace/AutoDocumentServiceApplication.java
@@ -0,0 +1,15 @@
+package auto.trace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class AutoDocumentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoDocumentServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/auto/trace/AutoDocumentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package auto.trace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoDocumentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
